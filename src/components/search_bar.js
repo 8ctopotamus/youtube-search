130edd44353feb201c1ffa72f2f28af4382ab4f2
Josh Naylor <8ctopotamus@gmail.com>
2016-09-04 15:28:49 -0500

    first commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import YoutubeSearch from 'youtube-api-search'
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' }
+  }
+
+  onInputChange(term) {
+    this.setState({term})
+    this.props.onSearchTermChange(term)
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
+      </div>
+    )
+  }
+}
+
+export default SearchBar
